510d0268c29a76163a52db2df059ce8b2ad29e39
christianbuendia-20 <cristianbr424@gmail.com>
2026-05-12 05:15:30 -0500

    El SEPTIMO  commit ZzZz

--- ecommerce/src/main/java/com/allprocess/ecommerce/repositories/UsuarioRepository.java
+++ ecommerce/src/main/java/com/allprocess/ecommerce/repositories/UsuarioRepository.java
@@ -3,16 +3,25 @@ package com.allprocess.ecommerce.repositories;
 import com.allprocess.ecommerce.entities.UsuarioENTITY;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioENTITY, Integer> {
 
-    // MAGIA DE SPRING: Solo con nombrar el método así, Spring Boot
-    // crea el SQL: SELECT * FROM usuario WHERE email = ?
+    // ==========================================
+    // PARA EL SISTEMA (Login y Registro)
+    // ==========================================
     Optional<UsuarioENTITY> findByEmail(String email);
-
-    // Para validar en el registro si el correo ya existe
     boolean existsByEmail(String email);
+
+    // ==========================================
+    // PARA EL PANEL DE ADMINISTRACIÓN (Tus listas)
+    // ==========================================
+
+    // Para ver solo usuarios activos o inactivos
+    List<UsuarioENTITY> findByActivo(Boolean activo);
+
+    // Para el buscador de la tabla (Buscar por nombre o apellido)
+    List<UsuarioENTITY> findByNombresContainingIgnoreCaseOrApellidosContainingIgnoreCase(String nombres, String apellidos);
 }
